c475175ee79fb4a761929d9aa527045ada7492e4
wanko <kouji124@w4.dion.ne.jp>
2020-10-08 03:15:20 +0900

    reduced variables

--- problem1.java
+++ problem1.java
@@ -4,30 +4,28 @@ public class problem1 {
 
     public static void main(String[] args) {
 
-        int max, number1, number2;
+        int tempMax, max, num1, num2;
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("Please enter 3 numbers: ");
-        int num1 = input.nextInt();
-        int num2 = input.nextInt();
-        int num3 = input.nextInt();
 
-        max = Math.max(num1, Math.max(num2, num3));
-
-        if (max == num1){
-            number1 = num2;
-            number2 = num3;
-        } else if (max == num2){
-            number1 = num1;
-            number2 = num3;
-        } else {
-            number1 = num1;
-            number2 = num2;
+        max = input.nextInt();
+        num1 = input.nextInt();
+        if(max < num1) {
+            tempMax = max;
+            max = num1;
+            num1 = tempMax;
+        }
+        num2 = input.nextInt();
+        if(max < num2) {
+            tempMax = max;
+            max = num2;
+            num2 = tempMax;
         }
 
-        if(max < number1 + number2) {
-            System.out.println("The perimeter is " + (max + number1 + number2));
+        if(max < num1 + num2) {
+            System.out.println("The perimeter is " + (max + num1 + num2));
         } else {
             System.out.println("The input is invalid");
         }
